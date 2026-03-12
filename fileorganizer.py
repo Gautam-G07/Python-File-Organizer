61198f74734220bcca4b72abe4207e72678b2ab7
Gautam G <gautam26g@gmail.com>
2026-03-12 21:59:04 +0530

    Final file organizer

--- fileorganizer.py
+++ fileorganizer.py
@@ -3,23 +3,24 @@ import shutil
 
 folder_path=r"C:\Users\Gautam G\OneDrive\Documents"
 files=os.listdir(folder_path)
-os.makedirs("Images", exist_ok=True)
-os.makedirs("Pdfs", exist_ok=True)
-os.makedirs("ppts", exist_ok=True)
-os.makedirs("Text", exist_ok=True)
-os.makedirs("Music", exist_ok=True)
+os.makedirs(os.path.join(folder_path, "Images"), exist_ok=True)
+os.makedirs(os.path.join(folder_path, "Pdfs"), exist_ok=True)
+os.makedirs(os.path.join(folder_path, "ppts"), exist_ok=True)
+os.makedirs(os.path.join(folder_path, "Text"), exist_ok=True)
+os.makedirs(os.path.join(folder_path, "Music"), exist_ok=True)
+
 for i in files:
     source = os.path.join(folder_path, i)
     if os.path.isfile(source):
         if i.endswith('.txt'):
-            shutil.move(source, 'Text')
+            shutil.move(source,os.path.join(folder_path, 'Text'))
         elif i.endswith('.pdf'):    
-            shutil.move(source, 'Pdfs')
+            shutil.move(source, os.path.join(folder_path, 'Pdfs'))
     
         elif i.endswith('.pptx'):
-            shutil.move(source, 'ppts')  
+            shutil.move(source, os.path.join(folder_path, 'ppts'))  
     
         elif i.endswith('.jpg') or i.endswith('.png'):
-            shutil.move(source, 'Images')
+            shutil.move(source, os.path.join(folder_path, 'Images'))
         elif i.endswith('.mp3'):
-            shutil.move(source, 'Music')
+            shutil.move(source, os.path.join(folder_path, 'Music'))
